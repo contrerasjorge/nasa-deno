40416456b0329e0d2987cf2d59e1c6f000d033e6
Jorge Contreras <jorge.contreras789@gmail.com>
2020-06-09 12:15:42 -0400

    added front-end code

--- mod.ts
+++ mod.ts
@@ -3,7 +3,20 @@ import { Application } from 'https://deno.land/x/oak@v5.0.0/mod.ts';
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get('X-Response=Time');
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set('X-Response-Time', `${delta}ms`);
+});
+
+app.use(async (ctx, next) => {
   ctx.response.body = `
     {___     {__      {_         {__ __        {_       
     {_ {__   {__     {_ __     {__    {__     {_ __     
@@ -13,6 +26,8 @@ app.use((ctx) => {
     {__    {_ __ {__       {__ {__    {__ {__       {__ 
     {__      {__{__         {__  {__ __  {__         {__
                     Mission Control API`;
+
+  await next();
 });
 
 if (import.meta.main) {
